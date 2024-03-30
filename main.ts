f5bc873922129477ccfff89d24498e517f162a1c
Bakhtawar khan <160532125+baki1996@users.noreply.github.com>
2024-03-30 12:47:36 +0500

    push my to do list to githup

---
+++ main.ts
@@ -0,0 +1,26 @@
+import inquirer from "inquirer"
+let todos = [];
+let condition = true;
+
+
+while(condition)
+{
+let addTask = await inquirer.prompt(
+    [
+      {
+          name:"todo",
+          type:"input",
+          message:"What you want to add in ur todos?",
+      },
+      {
+          name:"addMore",
+          type:"confirm",
+          message:"Do you to add more in that?",
+          default:"false",
+      },
+   ]
+);
+todos.push(addTask.todo);
+condition = addTask.addMore
+console.log(todos);
+}
